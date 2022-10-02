b84e61b96ed72728f4fcba05a413a63e5a07ba27
AnnShchuga <113831909+AnnShchuga@users.noreply.github.com>
2022-10-02 21:27:40 +0300

    Add files via upload

---
+++ ДЗ_4.py
@@ -0,0 +1,80 @@
+# Задание 1
+
+from math import pi
+
+d =  int(input("Введите число для заданной точности числа Pi:\n"))
+print(f'число Pi с заданной точностью {d} равно {round(pi, d)}')
+
+# Задание 2
+
+num = int(input("Введите число: "))
+i = 2
+lst = []
+old = num
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+print(f"Простые множители числа {old} приведены в списке: {lst}")
+
+# Задание 3
+
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Исходный список: {lst}")
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f"Список из неповторяющихся элементов: {new_lst}")
+
+# Задание 4
+
+import random
+
+def write_file(st):
+    with open('file04py.txt', 'w') as data:
+        data.write(st)
+
+def rnd():
+    return random.randint(0, 101)
+
+def create_mn(k):
+    lst = [rnd() for i in range(k + 1)]
+    return lst
+
+def create_str(sp):
+    lst = sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst) - i - 1}'
+                if lst[i + 1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i + 1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
+
+# Задание 5
+
+with open('file05py.txt', 'r') as data:
+    st2 = data.readlines()
+print(f"Первый многочлен {st1}")
+print(f"Второй многочлен {st2}")
+lst1 = calc_mn(st1)
+lst2 = calc_mn(st2)
+ll = len(lst1)
